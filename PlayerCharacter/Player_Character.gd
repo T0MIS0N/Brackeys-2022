2096b129dd72313bd3ef5593c78600ab4a68c062
DanielTStaggWSU <50230774+T0MIS0N@users.noreply.github.com>
2022-08-26 02:46:21 -0600

    Character Movement and Darkness
    Finished required functionality for player character movement. Created a test area for the character to walk around and made the area dark and textured the ground. Added small candle light to the character.

---
+++ PlayerCharacter/Player_Character.gd
@@ -0,0 +1,40 @@
+extends KinematicBody
+
+export var speed := 7.0
+export var gravity := 50.0
+
+var _velocity := Vector3.ZERO
+var _snap_vector := Vector3.DOWN
+
+onready var _spring_arm: SpringArm = $SpringArm
+onready var _model: Spatial = $Armature
+onready var _animation_player: AnimationPlayer = $AnimationPlayer
+
+func _physics_process(delta):
+	var move_direction = Vector3.ZERO
+	move_direction.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	move_direction.z = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	#This may be the camera direction code and may need to be removed for a static camera.
+	move_direction = move_direction.rotated(Vector3.UP, _spring_arm.rotation.y).normalized()
+	#This normalization is the same as above, but with rotation scrapped as that uses rotation adn the camera here is static
+	#move_direction = move_direction.normalized()
+	
+	_velocity.x = -move_direction.x * speed
+	_velocity.z = -move_direction.z * speed
+	_velocity.y -= gravity * delta
+	
+	#var just_landed = is_on_floor() and _snap_vector == Vector3.ZERO
+	
+	_snap_vector=Vector3.DOWN
+	
+	_velocity = move_and_slide_with_snap(_velocity, _snap_vector, Vector3.UP, true)
+	if _velocity.x == 0 and _velocity.z == 0:
+		_animation_player.play("Idle")
+	else:
+		_animation_player.play("Running")
+		$Armature.look_at(move_direction + translation, Vector3.UP)
+		$Armature.rotation_degrees.x = 0
+		$Armature.rotation_degrees.z = 0
+	
+#func _process(delta):
+#	_spring_arm.translation = translation
